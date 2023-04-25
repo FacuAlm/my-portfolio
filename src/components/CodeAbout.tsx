db855585c4b7f75e90cadb8fa998a0663440f346
FacuAlm <facundoalmaraz8@gmail.com>
2023-04-25 14:54:14 -0300

    Agregando componente sobre mi

---
+++ src/components/CodeAbout.tsx
@@ -0,0 +1,242 @@
+const CodeAbout = () => {
+  return (
+    <div className=" col-start-2 flex gap-3 ">
+      <div className="border w-[1px] "></div>
+
+      <div className="bg-black w-full rounded-2xl ">
+        <p className="text-gray-500 pt-3 pl-3">Sobre mi /{">"}</p>
+        <div className="p-10">
+          <p>
+            <span className="text-violet-500">export class</span>{" "}
+            <span className="text-yellow-500">FacundoAlmaraz</span>{" "}
+            <span className="text-white">{"{"}</span>
+          </p>
+
+          <p>
+            <span className="text-violet-500 ml-5">constructor</span>
+            <span className="text-white">()</span>{" "}
+            <span className="text-white">{"{"}</span>
+          </p>
+
+          <p>
+            <span className="text-violet-500 ml-10">this</span>
+            <span className="text-white">.</span>
+            <span className="text-yellow-500">name</span>
+            <span className="text-white"> = </span>
+            <span className="text-green-500">"Facundo Almaraz"</span>
+            <span className="text-white">;</span>
+          </p>
+
+          <p>
+            <span className="text-violet-500 ml-10">this</span>
+            <span className="text-white">.</span>
+            <span className="text-yellow-500">age</span>
+            <span className="text-white"> = </span>
+            <span className="text-green-500">"25"</span>
+            <span className="text-white">;</span>
+          </p>
+
+          <p>
+            <span className="text-violet-500 ml-10">this</span>
+            <span className="text-white">.</span>
+            <span className="text-yellow-500">location</span>
+            <span className="text-white"> = </span>
+            <span className="text-green-500">"Argentina"</span>
+            <span className="text-white">;</span>
+          </p>
+
+          <p>
+            <span className="text-violet-500 ml-10">this</span>
+            <span className="text-white">.</span>
+            <span className="text-yellow-500">email</span>
+            <span className="text-white"> = </span>
+            <span className="text-green-500">"facundoalmaraz8@gmail.com"</span>
+            <span className="text-white">;</span>
+          </p>
+
+          <p>
+            <span className="text-white ml-10">{"}"}</span>
+          </p>
+          <br />
+          <p>
+            <span className="text-violet-500 ml-5">educacion</span>
+            <span className="text-white">()</span>{" "}
+            <span className="text-white">{"{"}</span>
+          </p>
+
+          <p>
+            <span className="text-violet-500 ml-10">return</span>
+            <span className="text-white"> {"["}</span>
+          </p>
+
+          <p>
+            <span className="text-violet-500 ml-28">{"{"}</span>
+          </p>
+
+          <p>
+            <span className="text-violet-500 ml-36">Universidad</span>
+            <span className="text-white ">:</span>
+            <span className="text-green-500">
+              "Universidad de la Cuenca del Plata"
+            </span>
+            <span className="text-white">,</span>
+          </p>
+
+          <p>
+            <span className="text-violet-500 ml-36">titulo</span>
+            <span className="text-white">:</span>
+            <span className="text-green-500">
+              "Ingenieria en sistemas de Informacion"
+            </span>
+            <span className="text-white">,</span>
+          </p>
+
+          <p>
+            <span className="text-violet-500 ml-36">fecha</span>
+            <span className="text-white">:</span>
+            <span className="text-green-500">"2021 - Presente"</span>
+            <span className="text-white">,</span>
+          </p>
+
+          <p>
+            <span className="text-violet-500 ml-28">{"}"}</span>
+            <span className="text-white">,</span>
+          </p>
+
+          <p>
+            <span className="text-violet-500 ml-28">{"{"}</span>
+          </p>
+
+          <p>
+            <span className="text-violet-500 ml-36">Universidad</span>
+            <span className="text-white">:</span>
+            <span className="text-green-500">
+              "Universidad Tecnológica Nacional"
+            </span>
+            <span className="text-white">,</span>
+          </p>
+
+          <p>
+            <span className="text-violet-500 ml-36">titulo</span>
+            <span className="text-white">:</span>
+            <span className="text-green-500">
+              "Ingenieria en sistemas de Informacion"
+            </span>
+            <span className="text-white">,</span>
+          </p>
+
+          <p>
+            <span className="text-violet-500 ml-36">fecha</span>
+            <span className="text-white">:</span>
+            <span className="text-green-500">"2016-2019"</span>
+            <span className="text-white">,</span>
+          </p>
+
+          <p>
+            <span className="text-violet-500 ml-28">{"}"}</span>
+          </p>
+
+          <p>
+            <span className="text-white ml-10">{"]"}</span>
+          </p>
+
+          <p>
+            <span className="text-white ml-10">{"}"}</span>
+          </p>
+
+          <br />
+
+          <p>
+            <span className="text-violet-500 ml-5">habilidades</span>
+            <span className="text-white">()</span>{" "}
+            <span className="text-white">{"{"}</span>
+          </p>
+
+          <p>
+            <span className="text-violet-500 ml-10">return</span>
+            <span className="text-white"> {"["}</span>
+          </p>
+
+          <p>
+            <span className="text-violet-500 ml-28">{"'"}</span>
+            <span className="text-green-500">HTML</span>
+            <span className="text-violet-500">{"'"}</span>
+            <span className="text-white">,</span>
+          </p>
+
+          <p>
+            <span className="text-violet-500 ml-28">{"'"}</span>
+            <span className="text-green-500">CSS</span>
+            <span className="text-violet-500">{"'"}</span>
+            <span className="text-white">,</span>
+          </p>
+
+          <p>
+            <span className="text-violet-500 ml-28">{"'"}</span>
+            <span className="text-green-500">Javascript</span>
+            <span className="text-violet-500">{"'"}</span>
+            <span className="text-white">,</span>
+          </p>
+
+          <p>
+            <span className="text-violet-500 ml-28">{"'"}</span>
+            <span className="text-green-500">ReactJS</span>
+            <span className="text-violet-500">{"'"}</span>
+            <span className="text-white">,</span>
+          </p>
+
+          <p>
+            <span className="text-violet-500 ml-28">{"'"}</span>
+            <span className="text-green-500">ExpressJS</span>
+            <span className="text-violet-500">{"'"}</span>
+            <span className="text-white">,</span>
+          </p>
+
+          <p>
+            <span className="text-violet-500 ml-28">{"'"}</span>
+            <span className="text-green-500">MongoDB</span>
+            <span className="text-violet-500">{"'"}</span>
+            <span className="text-white">,</span>
+          </p>
+
+          <p>
+            <span className="text-violet-500 ml-28">{"'"}</span>
+            <span className="text-green-500">SQL</span>
+            <span className="text-violet-500">{"'"}</span>
+            <span className="text-white">,</span>
+          </p>
+
+          <p>
+            <span className="text-violet-500 ml-28">{"'"}</span>
+            <span className="text-green-500">React Native</span>
+            <span className="text-violet-500">{"'"}</span>
+            <span className="text-white">,</span>
+          </p>
+
+          <p>
+            <span className="text-violet-500 ml-28">{"'"}</span>
+            <span className="text-green-500">Tailwindcss</span>
+            <span className="text-violet-500">{"'"}</span>
+            <span className="text-white">,</span>
+          </p>
+
+          <p>
+            <span className="text-white ml-28">{"]"}</span>
+          </p>
+
+          <p>
+            <span className="text-white ml-10">{"}"}</span>
+          </p>
+
+          <br />
+
+          <p>
+            <span className="text-white ml-5">{"}"}</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CodeAbout;
